ebac4fc6fc580663cad2b44ebc5018b27ef1de39
umamkhairul723 <umamkhairul723@gmail.com>
2026-05-05 12:09:22 +0700

    feat: tambah halaman detail jurumiyah

--- lib/data/jurumiyah_data.dart
+++ lib/data/jurumiyah_data.dart
@@ -4,11 +4,11 @@ List<Bab> jurumiyahList = [
   Bab(
     judul: "Kalam",
     arab: "الكلام هو اللفظ المركب المفيد بالوضع",
-    arti: "Kalam adalah lafadz yang tersusun dan memberi faidah.",
+    arti: "Kalam adalah lafadz yang tersusun dan memberi faidah dan whedok .",
   ),
   Bab(
     judul: "Isim",
     arab: "الاسم ما دل على معنى في نفسه",
-    arti: "Isim adalah kata yang menunjukkan makna.",
+    arti: "Isim adalah kata yang menunjukkan makna nama .",
   ),
 ];
