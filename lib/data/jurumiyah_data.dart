c7a826dcb10775c864f5c526580e0dc7234b8b28
umamkhairul723 <umamkhairul723@gmail.com>
2026-05-12 09:29:50 +0700

    menambahkan datan

--- lib/data/jurumiyah_data.dart
+++ lib/data/jurumiyah_data.dart
@@ -7,8 +7,23 @@ List<Bab> jurumiyahList = [
     arti: "Kalam adalah lafadz yang tersusun dan memberi faidah dan whedok .",
   ),
   Bab(
-    judul: "Isim",
+    judul: "kalimat Isim",
     arab: "الاسم ما دل على معنى في نفسه",
     arti: "Isim adalah kata yang menunjukkan makna nama .",
+  ),
+  Bab(
+    judul: "kalimat fi'il",
+    arab: "الفِعْلُ مَا دَلَّ عَلَى مَعْنًى فِي نَفْسِهِ وَاقْتَرَنَ بِزَمَانٍ",
+    arti: "Fi‘il adalah kata yang menunjukkan makna pada dirinya sendiri dan berkaitan dengan waktu .",
+  ),
+  Bab(
+    judul: "kalimat huruf",
+    arab: "الحَرْفُ مَا دَلَّ عَلَى مَعْنًى فِي غَيْرِهِ",
+    arti: "Huruf adalah kata yang menunjukkan suatu makna yang tidak sempurna kecuali jika bersama kalimat lain .",
+  ),
+  Bab(
+    judul: "i'rab",
+    arab: "لإِعْرَابُ هُوَ تَغْيِيرُ أَوَاخِرِ الْكَلِمِ لاِخْتِلَافِ الْعَوَامِلِ الدَّاخِلَةِ عَلَيْهَا لَفْظًا أَوْ تَقْدِيرًا",
+    arti: "I‘rab adalah perubahan pada akhir kalimat karena perbedaan ‘amil (faktor yang memengaruhi) yang masuk kepadanya, baik secara lafad maupun secara perkiraan .",
   ),
 ];
